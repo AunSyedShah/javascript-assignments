8c01eb0a195da22dec24a3501f04dd13977c142c
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2022-09-30 11:22:35 +0500

    initial commit

---
+++ assgn_3/assgn_3.js
@@ -0,0 +1,6 @@
+const person_name = "Syed Aun Muhammad";
+// print person_name in upper case, lower case
+console.log(person_name.toUpperCase());
+console.log(person_name.toLowerCase());
+// convert person_name to title case
+console.log(person_name.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}));
---
+++ assgn_5/assgn_5.js
@@ -0,0 +1,3 @@
+const author = "Imam Ali";
+const quote = "He who adopts greed as a habit devalues himself; he who discloses his hardship agrees to humiliation; and he who allows his tongue to overpower his soul debases the soul.";
+console.log(`${author} said, "${quote}"`);
